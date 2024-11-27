dd791d3293af1446bde7e2d2bc721abb608c4f81
Ali Mirghasemi <ali.mirghasemi1376@gmail.com>
2024-11-27 14:59:26 +0330

    Start replacing queue backend

--- Src/Queue.h
+++ Src/Queue.h
@@ -1,8 +1,8 @@
 /**
  * @file Queue.h
  * @author Ali Mirghasemi (ali.mirghasemi1376@gmail.com)
- * @brief this library implement queue buffer with read & write operations
- * @version 0.5
+ * @brief this library implement queue buffer with read & write operations, it's based n Stream library
+ * @version 0.2
  * @date 2021-09-01
  *
  * @copyright Copyright (c) 2021
@@ -15,10 +15,10 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
+#include "StreamBuffer.h"
 
 #define QUEUE_VER_MAJOR    0
-#define QUEUE_VER_MINOR    1
+#define QUEUE_VER_MINOR    2
 #define QUEUE_VER_FIX      0
 
 /************************************************************************/
@@ -58,7 +58,7 @@ extern "C" {
  * you can change type of len variables
  * must be signed type
  */
-typedef int16_t Queue_LenType;
+typedef Stream_LenType Queue_LenType;
 
 /************************************************************************/
 
@@ -76,40 +76,34 @@ typedef int16_t Queue_LenType;
 /**
  * @brief use for disable limit
  */
-#define QUEUE_NO_LIMIT                     -1
+#define QUEUE_NO_LIMIT                          STREAM_NO_LIMIT
 
 /**
  * @brief result of Queue functions
  */
 typedef enum {
-    Queue_Ok                = 0,    /**< everything is OK */
-    Queue_NoSpace           = 1,    /**< there is no space for write operation */
-    Queue_NoAvailable       = 2,    /**< there no available bytes for read operation */
-    Queue_BufferFull        = 3,    /**< buffer full*/
-    Queue_ZeroLen           = 4,   /**< len parameter is zero */
+    Queue_Ok               = Stream_Ok,                 /**< everything is OK */
+    Queue_NoSpace          = Stream_NoSpace,            /**< there is no space for write operation */
+    Queue_NoAvailable      = Stream_NoAvailable,        /**< there no available bytes for read operation */
+    Queue_BufferFull       = Stream_BufferFull,         /**< buffer full*/
+    Queue_NoReceiveFn      = Stream_NoReceiveFn,        /**< no receive function set for IStream */
+    Queue_NoTransmitFn     = Stream_NoTransmitFn,       /**< no transmit function set for OStream */
+    Queue_NoReceive        = Stream_NoReceive,          /**< stream is not in receive mode */
+    Queue_NoTransmit       = Stream_NoTransmit,         /**< stream is not in transmit mode */
+    Queue_InReceive        = Stream_InReceive,          /**< stream is in receive mode */
+    Queue_InTransmit       = Stream_InTransmit,         /**< stream is in transmit mode */
+    Queue_ZeroLen          = Stream_ZeroLen,            /**< len parameter is zero */
+    Queue_ReceiveFailed    = Stream_ReceiveFailed,      /**< failed in receive */
+    Queue_TransmitFailed   = Stream_TransmitFailed,     /**< failed in transmit */
+    Queue_CustomError      = Stream_CustomError,        /**< can be used for custom errors */
 } Queue_Result;
 /**
  * @brief Queue struct
  * contains everything need for handle queue
  */
 typedef struct {
-    uint8_t*                Buf;                    /**< pointer to buffer */
-    Queue_LenType           Size;                   /**< size of buffer */
-    Queue_LenType           WPos;                   /**< write position */
-    Queue_LenType           RPos;                   /**< read position */
-    Queue_LenType           ItemSize;               /**< length of each item */
-#if QUEUE_WRITE_LIMIT
-    Queue_LenType           WriteLimit;             /**< limit for write operation */
-#endif 
-#if QUEUE_READ_LIMIT
-    Queue_LenType           ReadLimit;              /**< limit for read operation */
-#endif
-    uint8_t                 Overflow    : 1;        /**< overflow flag */
-    uint8_t                 Order       : 1;        /**< byte order */
-    uint8_t                 OrderFn     : 1;        /**< byte order function */
-    uint8_t                 WriteLocked : 1;        /**< queue write locked */
-    uint8_t                 ReadLocked  : 1;        /**< queue write locked */
-    uint8_t                 Reserved    : 3;        /**< reserved */
+    StreamBuffer            Buffer;                     /**< Queue buffer */
+    Queue_LenType           ItemLength;                 /**< length of each item */
 } Queue;
 /**
  * @brief Write or Read queue with custom functions as query
@@ -121,77 +115,91 @@ typedef struct {
  */
 typedef Queue_Result (*Queue_QueryFn)(Queue* queue, void* val, Queue_LenType index, Queue_LenType len);
 
-void Queue_init(Queue* queue, void* buffer, Queue_LenType size, Queue_LenType itemLength);
-void Queue_fromBuff(Queue* queue, void* buffer, Queue_LenType size, Queue_LenType itemLength, Queue_LenType len);
-void Queue_deinit(Queue* queue);
+// -------------------------- General APIs ----------------------------
+void                Queue_init(Queue* queue, void* buffer, Queue_LenType size, Queue_LenType itemLength);
+void                Queue_fromBuff(Queue* queue, void* buffer, Queue_LenType size, Queue_LenType itemLength, Queue_LenType len);
+void                Queue_deinit(Queue* queue);
 
-/*************** General APIs *************/
+#define             Queue_space(QUEUE)                                      (Queue_spaceRaw(QUEUE) / ((QUEUE)->ItemLength))
+#define             Queue_available(QUEUE)                                  (Queue_availableRaw(QUEUE) / ((QUEUE)->ItemLength))
 
+#define             Queue_isEmpty(QUEUE)                                    Stream_isEmpty(&((QUEUE)->Buffer))
+#define             Queue_isFull(QUEUE)                                     Stream_isFull(&((QUEUE)->Buffer))
+
+#define             Queue_directAvailable(QUEUE)                            (Queue_directAvailableRaw(QUEUE) / ((QUEUE)->ItemLength))
+#define             Queue_directSpace(QUEUE)                                (Queue_directSpaceRaw(QUEUE) / ((QUEUE)->ItemLength))
+
+#define             Queue_directAvailableAt(QUEUE, IDX)                     (Queue_directAvailableAtRaw(QUEUE, IDX) / ((QUEUE)->ItemLength))
+#define             Queue_directSpaceAt(QUEUE, IDX)                         (Queue_directSpaceAtRaw(QUEUE, IDX) / ((QUEUE)->ItemLength))
+
+#define             Queue_getWritePtr(QUEUE)                                ((void*) Stream_getWritePtr(&((QUEUE)->Buffer)))
+#define             Queue_getReadPtr(QUEUE)                                 ((void*) Stream_getReadPtr(&((QUEUE)->Buffer)))
+#define             Queue_getWritePtrAt(QUEUE, IDX)                         ((void*) Stream_getWritePtrAt(&((QUEUE)->Buffer), (IDX) * ((QUEUE)->ItemLength)))
+#define             Queue_getReadPtrAt(QUEUE, IDX)                          ((void*) Stream_getReadPtrAt(&((QUEUE)->Buffer), (IDX) * ((QUEUE)->ItemLength)))
+
+#define             Queue_reset(QUEUE)                                      Stream_reset(&((QUEUE)->Buffer))
+#define             Queue_clear(QUEUE)                                      Stream_clear(&((QUEUE)->Buffer))
+
+#define             Queue_setBuffer(QUEUE, BUF)                             Stream_getBuffer(&((QUEUE)->Buffer), (BUF))
+#define             Queue_getBuffer(QUEUE)                                  ((void*) Stream_getBuffer(&((QUEUE)->Buffer)))
+#define             Queue_getBufferSize(QUEUE)                              (Queue_getBufferSizeRaw((QUEUE)) / ((QUEUE)->ItemLength))
+
+#define             Queue_getWritePos(QUEUE)                                (Queue_getWritePosRaw(QUEUE) / ((QUEUE)->ItemLength))
+#define             Queue_getReadPos(QUEUE)                                 (Queue_getReadPosRaw(QUEUE) / ((QUEUE)->ItemLength))
+
+#define             Queue_moveWritePos(QUEUE, STEPS)                        Queue_moveReadPosRaw(QUEUE, STEPS * ((QUEUE)->ItemLength))
+#define             Queue_moveReadPos(QUEUE, STEPS)                         Queue_moveWritePosRaw(QUEUE, STEPS * ((QUEUE)->ItemLength))
+
+#define             Queue_flipWrite(QUEUE, LEN)                             Queue_flipWriteRaw(QUEUE, (LEN) * ((QUEUE)->ItemLength))
+#define             Queue_flipRead(QUEUE, LEN)                              Queue_flipReadRaw(QUEUE, (LEN) * ((QUEUE)->ItemLength))
+
+
+// -------------------------- Raw APIs ----------------------------
+#define             Queue_spaceRaw(QUEUE)                                   Stream_space(&((QUEUE)->Buffer))
+#define             Queue_availableRaw(QUEUE)                               Stream_available(&((QUEUE)->Buffer))
+
+#define             Queue_directAvailableRaw(QUEUE)                         Stream_directAvailable(&((QUEUE)->Buffer))
+#define             Queue_directSpaceRaw(QUEUE)                             Stream_directSpace(&((QUEUE)->Buffer))
+
+#define             Queue_directAvailableAtRaw(QUEUE, IDX)                  Stream_directAvailableAt(&((QUEUE)->Buffer), (IDX))
+#define             Queue_directSpaceAtRaw(QUEUE, IDX)                      Stream_directSpaceAt(&((QUEUE)->Buffer), (IDX))
+
+#define             Queue_getWritePosRaw(QUEUE)                             Stream_getWritePos(&((QUEUE)->Buffer))
+#define             Queue_getReadPosRaw(QUEUE)                              Stream_getReadPos(&((QUEUE)->Buffer))
+
+#define             Queue_moveWritePosRaw(QUEUE, STEPS)                     Stream_moveWritePos(&((QUEUE)->Buffer), (STEPS))
+#define             Queue_moveReadPosRaw(QUEUE, STEPS)                      Stream_moveReadPos(&((QUEUE)->Buffer), (STEPS))
+
+#define             Queue_flipWriteRaw(QUEUE, LEN)                          Stream_flipWrite(&((QUEUE)->Buffer), (LEN))
+#define             Queue_flipReadRaw(QUEUE, LEN)                           Stream_flipRead(&((QUEUE)->Buffer), (LEN))
+
+#define             Queue_getBufferSizeRaw(QUEUE)                           Stream_getBufferSize(&((QUEUE)->Buffer))
+
+// -------------------------- Write Limit APIs ----------------------------
 #if QUEUE_WRITE_LIMIT
-    #define Queue_spaceRaw(QUEUE)                Queue_spaceLimitRaw((QUEUE))
-#else
-    #define Queue_spaceRaw(QUEUE)                Queue_spaceRealRaw((QUEUE))
+    #define         Queue_spaceLimitRaw(QUEUE)                              Stream_spaceLimit(&((QUEUE)->Buffer))
+    #define         Queue_getWriteLimitRaw(QUEUE)                           Stream_getWriteLimit(&((QUEUE)->Buffer))
+    #define         Queue_setWriteLimitRaw(QUEUE, LEN)                      Stream_setWriteLimit(&((QUEUE)->Buffer), (LEN))
+    
+    #define         Queue_spaceLimit(QUEUE)                                 (Queue_spaceLimitRaw(QUEUE) / ((QUEUE)->ItemLength))
+    #define         Queue_getWriteLimit(QUEUE)                              (Queue_getWriteLimitRaw(QUEUE) / ((QUEUE)->ItemLength))
+    #define         Queue_setWriteLimit(QUEUE, LEN)                         Queue_setWriteLimitRaw(QUEUE, (LEN) * ((QUEUE)->ItemLength))
+    #define         Queue_isWriteLimited(QUEUE)                             Stream_isWriteLimited(&((QUEUE)->Buffer))
 #endif // QUEUE_WRITE_LIMIT
 
+// -------------------------- Read Limit APIs ----------------------------
 #if QUEUE_READ_LIMIT
-    #define Queue_availableRaw(QUEUE)            Queue_availableLimitRaw((QUEUE))
-#else
-    #define Queue_availableRaw(QUEUE)            Queue_availableRealRaw((QUEUE))
+    #define         Queue_availableLimitRaw(QUEUE)                          Stream_availableLimit(&((QUEUE)->Buffer))
+    #define         Queue_getReadLimitRaw(QUEUE)                            Stream_getReadLimit(&((QUEUE)->Buffer))
+    #define         Queue_setReadLimitRaw(QUEUE, LEN)                       Stream_setReadLimit(&((QUEUE)->Buffer), (LEN))
+    
+    #define         Queue_availableLimit(QUEUE)                             (Queue_availableLimitRaw(QUEUE) / ((QUEUE)->ItemLength))
+    #define         Queue_getReadLimit(QUEUE)                               (Queue_getReadLimitRaw(QUEUE) / ((QUEUE)->ItemLength))
+    #define         Queue_setReadLimit(QUEUE, LEN)                          Queue_setReadLimitRaw(QUEUE, (LEN) * ((QUEUE)->ItemLength))
+    #define         Queue_isReadLimited(QUEUE)                              Stream_isReadLimited(&((QUEUE)->Buffer))
 #endif // QUEUE_READ_LIMIT
 
-Queue_LenType Queue_availableRealRaw(Queue* queue);
-Queue_LenType Queue_spaceRealRaw(Queue* queue);
-Queue_LenType Queue_available(Queue* queue);
-Queue_LenType Queue_space(Queue* queue);
-uint8_t Queue_isEmpty(Queue* queue);
-uint8_t Queue_isFull(Queue* queue);
-
-Queue_LenType Queue_directAvailableRaw(Queue* queue);
-Queue_LenType Queue_directSpaceRaw(Queue* queue);
-
-Queue_LenType Queue_directAvailableAtRaw(Queue* queue, Queue_LenType index);
-Queue_LenType Queue_directSpaceAtRaw(Queue* queue, Queue_LenType index);
-
-void* Queue_getWritePtr(Queue* queue);
-void* Queue_getReadPtr(Queue* queue);
-void* Queue_getWritePtrAt(Queue* queue, Queue_LenType index);
-void* Queue_getReadPtrAt(Queue* queue, Queue_LenType index);
-
-void Queue_reset(Queue* queue);
-void Queue_clear(Queue* queue);
-
-uint8_t* Queue_getBuffer(Queue* queue);
-
-void Queue_setBuffer(Queue* queue, void* data, Queue_LenType size, Queue_LenType itemSize);
-Queue_LenType Queue_getBufferSize(Queue* queue);
-
-Queue_LenType Queue_getWritePos(Queue* queue);
-Queue_LenType Queue_getReadPos(Queue* queue);
-
-Queue_Result Queue_moveWritePosRaw(Queue* queue, Queue_LenType steps);
-Queue_Result Queue_moveReadPosRaw(Queue* queue, Queue_LenType steps);
-
-void Queue_flipWrite(Queue* queue, Queue_LenType len);
-void Queue_flipRead(Queue* queue, Queue_LenType len);
-
-#if QUEUE_WRITE_LIMIT
-    void       Queue_setWriteLimit(Queue* queue, Queue_LenType len);
-    uint8_t    Queue_isWriteLimited(Queue* queue);
-    Queue_LenType Queue_spaceLimitRaw(Queue* queue);
-    Queue_LenType Queue_getWriteLimitRaw(Queue* queue);
-    Queue_LenType Queue_spaceLimit(Queue* queue);
-    Queue_LenType Queue_getWriteLimit(Queue* queue);
-#endif // QUEUE_WRITE_LIMIT
-
-#if QUEUE_READ_LIMIT
-    void       Queue_setReadLimit(Queue* queue, Queue_LenType len);
-    uint8_t    Queue_isReadLimited(Queue* queue);
-    Queue_LenType Queue_availableLimitRaw(Queue* queue);
-    Queue_LenType Queue_getReadLimitRaw(Queue* queue);
-    Queue_LenType Queue_availableLimit(Queue* queue);
-    Queue_LenType Queue_getReadLimit(Queue* queue);
-#endif // QUEUE_READ_LIMIT
-
+// -------------------------- Write Lock APIs ----------------------------
 #if QUEUE_WRITE_LOCK
     Queue_Result    Queue_lockWrite(Queue* queue, Queue* lock, Queue_LenType len);
     void            Queue_unlockWrite(Queue* queue, Queue* lock);
